bed6f66dc008faf64253a09f41f57086cee31549
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-03-03 22:03:08 +0300

    Project Created

---
+++ CatchKenny/CatchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Akın Aksoy on 3.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
